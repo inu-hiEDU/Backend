87b2a8d2b17cf943f198cbc0e12d2b28fa637075
yuripbong <kyri200350@gmail.com>
2025-03-26 15:35:19 +0900

    [set] MySQL 연결 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
+import { typeOrmConfig } from './\bconfig/typeOrm.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(), // .env 파일 로드
+    TypeOrmModule.forRoot(typeOrmConfig),
+    UsersModule,
+  ],
   controllers: [],
   providers: [],
 })
