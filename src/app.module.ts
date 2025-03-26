2cb212cea5add5260699bd6f5d5f5a764277334c
yuripbong <kyri200350@gmail.com>
2025-03-26 15:46:49 +0900

    [remove] user 관련 코드 삭제

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
-import { UsersModule } from './users/users.module';
 import { typeOrmConfig } from './\bconfig/typeOrm.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot(), // .env 파일 로드
     TypeOrmModule.forRoot(typeOrmConfig),
-    UsersModule,
   ],
   controllers: [],
   providers: [],
