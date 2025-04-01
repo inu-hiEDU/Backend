cc02fde392591d13ee789e4c685faa0f4c026edd
paulleebm <paulleebm@naver.com>
2025-04-01 20:48:47 +0900

    [feat/#15] 학생정보CRUD
    [feat/#15] 학생정보CRUD

--- src/students/student.controller.ts
+++ src/students/student.controller.ts
@@ -1,6 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Patch,
+  Delete,
+  Param,
+  Body,
+  ParseIntPipe,
+  HttpCode,
+} from '@nestjs/common';
+
 import { StudentService } from './student.service';
 import { Student } from './student.entity';
+import { CreateStudentDto } from './dto/create-student.dto';
+import { UpdateStudentDto } from './dto/update-student.dto';
 
 @Controller('students')
 export class StudentController {
@@ -9,5 +22,25 @@ export class StudentController {
   @Get()
   async getAllStudents(): Promise<Student[]> {
     return this.studentService.getAllStudents();
+  }
+  @Get(':id')
+  async getStudent(@Param('id', ParseIntPipe) id: number) {
+    return this.studentService.getStudentById(id);
+  }
+
+  @Post()
+  async createStudent(@Body() dto: CreateStudentDto) {
+    return this.studentService.createStudent(dto);
+  }
+
+  @Patch(':id')
+  async updateStudent(@Param('id', ParseIntPipe) id: number, @Body() dto: UpdateStudentDto) {
+    return this.studentService.updateStudent(id, dto);
+  }
+
+  @Delete(':id')
+  @HttpCode(204)
+  async deleteStudent(@Param('id', ParseIntPipe) id: number) {
+    return this.studentService.deleteStudent(id);
   }
 }
--- src/students/student.repository.ts
+++ src/students/student.repository.ts
@@ -13,4 +13,22 @@ export class StudentRepository {
   async findAll(): Promise<Student[]> {
     return this.studentRepo.find();
   }
+
+  async findOneById(id: number): Promise<Student> {
+    return this.studentRepo.findOneByOrFail({ id });
+  }
+
+  async createStudent(dto: Partial<Student>): Promise<Student> {
+    const student = this.studentRepo.create(dto);
+    return this.studentRepo.save(student);
+  }
+
+  async updateStudent(id: number, dto: Partial<Student>): Promise<Student> {
+    await this.studentRepo.update(id, dto);
+    return this.findOneById(id);
+  }
+
+  async deleteStudent(id: number): Promise<void> {
+    await this.studentRepo.delete(id);
+  }
 }
