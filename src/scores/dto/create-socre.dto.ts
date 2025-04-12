ccdee7031ec2d72eec7b14a280dbfef089e674de
yuripbong <kyri200350@gmail.com>
2025-04-12 23:02:02 +0900

    feat: dto 생성

---
+++ src/scores/dto/create-socre.dto.ts
@@ -0,0 +1,13 @@
+export class CreateOrUpdateScoreDto {
+  studentId: number;
+  grade: number;
+  semester: number;
+  subject1?: number;
+  subject2?: number;
+  subject3?: number;
+  subject4?: number;
+  subject5?: number;
+  subject6?: number;
+  subject7?: number;
+  subject8?: number;
+}
