96264a1cb8bb1495520c6066fcca007c66c05a1b
yuripbong <kyri200350@gmail.com>
2025-04-20 21:21:07 +0900

    comment: 80

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 80);
-  console.log('Servcer is running on http://localhost:3011');
+  console.log('Servcer is running on http://localhost:80');
 }
 bootstrap();
