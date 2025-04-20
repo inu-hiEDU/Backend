bc7edf1e5703f356224f92cdb511a507eee47c01
yuripbong <kyri200350@gmail.com>
2025-04-20 21:20:26 +0900

    chore: 포트 3011 -> 80 변경

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3011);
+  await app.listen(process.env.PORT ?? 80);
   console.log('Servcer is running on http://localhost:3011');
 }
 bootstrap();
