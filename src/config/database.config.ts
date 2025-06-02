9fd5a8aed3d098a895276735e6832c61f9b8b0a2
yuripbong <kyri200350@gmail.com>
2025-06-03 00:03:38 +0900

    set: 도커 컴포즈 파일 mysql 수정

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -3,13 +3,13 @@ import { ConfigService } from '@nestjs/config';
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { Attendance } from 'src/attendance/attendance.entity';
 import { Counsel } from 'src/counsel/counsel.entity';
+import { Feedback } from 'src/feedback/feedback.entity';
 import { Login } from 'src/login/login.entity';
+import { Parent } from 'src/parents/parent.entity';
 import { Scores } from 'src/scores/score.entity';
 import { Student } from 'src/students/student.entity';
-import { User } from 'src/user/user.entity';
-import { Parent } from 'src/parents/parent.entity';
 import { Teacher } from 'src/teachers/teacher.entity';
-import { Feedback } from 'src/feedback/feedback.entity';
+import { User } from 'src/user/user.entity';
 
 @Injectable()
 export class DatabaseConfigService {
@@ -25,8 +25,17 @@ export class DatabaseConfigService {
       database: this.configService.get<string>('DB_DATABASE'),
       synchronize: true, // 개발 환경에서만 true로 설정
       logging: true, // 로그 확인용
-      entities: [Student, Login, Attendance, Counsel, Scores,
-        User, Parent, Teacher, Feedback],
+      entities: [
+        Student,
+        Login,
+        Attendance,
+        Counsel,
+        Scores,
+        User,
+        Parent,
+        Teacher,
+        Feedback,
+      ],
     };
   }
 }
