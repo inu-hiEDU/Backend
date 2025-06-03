52588bb4f1ccca4cd9f6c9a06ace045f459baea5
paulleebm <paulleebm@naver.com>
2025-06-03 14:05:14 +0900

    [feat/#98]

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -9,9 +9,25 @@ import { TeacherModule } from 'src/teachers/teacher.module';
 import { StudentModule } from 'src/students/student.module';
 import { ParentModule } from 'src/parents/parent.module';
 import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [UserModule, PassportModule, TeacherModule,StudentModule, ParentModule,],
+  imports: [
+    ConfigModule,
+    PassportModule,
+    UserModule,
+    ParentModule,
+    StudentModule,
+    TeacherModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule], 
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: '1d' },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService, GoogleStrategy, JwtStrategy],
   exports: [AuthService, JwtModule],
